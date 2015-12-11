54279a55596fb2dcf23823b911c2438c27d3f18c
rodney.odvina <rodvina@kemper.com>
2015-12-11 13:28:25 -0500

    Cleanup, remove unneeded groovy files

--- rest-customerpolicy/src/main/groovy/com/kemper/customer/rest/UserController.groovy
+++ rest-customerpolicy/src/main/groovy/com/kemper/customer/rest/UserController.groovy
@@ -1,13 +1,11 @@
 package com.kemper.customer.rest;
 
 
-import java.lang.reflect.Method;
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -50,7 +48,7 @@ public class UserController {
 	}
 				
 	@RequestMapping(value = "/users/{userId}/policies/{policyNum}/drivers/{driverId}",  method = RequestMethod.PUT)
-	public Policy updateDriver(@PathVariable String userId, @PathVariable String policyNum, 
+	def updateDriver(@PathVariable String userId, @PathVariable String policyNum, 
 								@PathVariable String driverId, @RequestBody def driver) {
 		return userRepository.updateDriver(policyNum, driver) 
 	}
