a6ef87c328d724d2e3117f38af3ca9c8d6030464
rodney.odvina <rodvina@kemper.com>
2015-12-15 14:26:46 -0500

    Enabled CORS for localhost:9000

--- rest-customerpolicy/src/main/groovy/com/kemper/customer/rest/UserController.groovy
+++ rest-customerpolicy/src/main/groovy/com/kemper/customer/rest/UserController.groovy
@@ -2,12 +2,14 @@ package com.kemper.customer.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 public class UserController {
 	
